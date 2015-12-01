685d6b7af9e154d6cdef8331830375b968338f10
AJPersinger <am3798@drexel.edu>
2015-11-30 19:15:33 -0500

    Modified code to only check 100 words and output to file

--- pdfRW.py
+++ pdfRW.py
@@ -20,6 +20,7 @@ dict1 = {}
 dict2 = {}
 dict3 = {}
 dict4 = {}
+dictionary = {}
 finalDefList = [dict0, dict1, dict2, dict3, dict4]
 k = 0
 i = 0
@@ -31,10 +32,14 @@ i = 0
 #            dictionary[word] = wikipedia.summary(word, sentences=2)
 #        k += 1
 string = open('words.txt').read()
-for word in string.split():
-    dictionary[word] = wikipedia.summary(word, sentences=2)
+while i < 100:
+    for word in string.split():
+        dictionary[word] = wikipedia.summary(word, sentences=2)
+        i += 1
 
-
+f = open(output, 'w' )
+f.write(dictionary)
+f.close()
 
 
 print(time.time() - t)
