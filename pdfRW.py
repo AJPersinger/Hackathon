f4e5feb5cb6f6133c7d0c685c43627f235bc6fcc
AJPersinger <am3798@drexel.edu>
2015-11-30 19:07:51 -0500

    Modified Code for new wordlist

--- pdfRW.py
+++ pdfRW.py
@@ -11,7 +11,7 @@ definitionList = []
 #outputFile = open("outputFile.txt", "wb")
 outputList = []
 totalWords = 0
-string = open('4.txt').read()
+string = open('words.txt').read()
 for word in string.split():
     totalWords += 1
 
@@ -23,13 +23,17 @@ dict4 = {}
 finalDefList = [dict0, dict1, dict2, dict3, dict4]
 k = 0
 i = 0
-while i < len(finalDefList):
-    for dictionary in finalDefList:
-        i += 1
-        string = open(str(k)+'.txt').read()
-        for word in string.split():
-            dictionary[word] = wikipedia.summary(word, sentences=2)
-        k += 1
+#while i < len(finalDefList):
+#    for dictionary in finalDefList:
+#        i += 1
+#        string = open(str(k)+'.txt').read()
+#        for word in string.split():
+#            dictionary[word] = wikipedia.summary(word, sentences=2)
+#        k += 1
+string = open('words.txt').read()
+for word in string.split():
+    dictionary[word] = wikipedia.summary(word, sentences=2)
+
 
 
 
